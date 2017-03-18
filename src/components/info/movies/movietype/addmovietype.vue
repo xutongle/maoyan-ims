005d342c8e30c0697a484c52551ae10209976538
Dawn <605722309@qq.com>
2017-03-18 12:16:35 +0800

    代码报错 安装stage-3 后恢复当前代码

--- src/components/info/movies/movietype/addmovietype.vue
+++ src/components/info/movies/movietype/addmovietype.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <template>
 	<div class='content'>
 		<el-form :model="movietypeText"  :rules="rules" ref="formName"  label-width="100px" >
@@ -26,25 +25,9 @@
 		  </el-form-item>
 		</el-form>
    </div>
-=======
-
-<template>
-	<div>
-		<el-form :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-width="100px" class="demo-ruleForm">
-		  <el-form-item label="添加电影类型" label-color="#1F2D3D" prop="type">
-		    <el-input type="text" v-model="ruleForm2.types" auto-complete="off"></el-input>
-		  </el-form-item>
-		 
-		  <el-form-item>
-		    <el-button type="primary" @click="submitForm('ruleForm2')">保存</el-button>
-		    <el-button type="primary" @click="resetForm('ruleForm2')">重置</el-button>
-		  </el-form-item>
-		</el-form>
-	</div>
->>>>>>> parent of abc025a... addmovietype页面完成提交
 </template>
+
 <script>
-<<<<<<< HEAD
 
   export default {
     data() {
@@ -107,34 +90,11 @@
 		background:#fff;
 		border:1px solid #3879D9;
 	}
-=======
-	export default {
-	  data() {
-	       return {
-	         ruleForm2: {
-	           types: '',
-	         },
-	         rules2: {
-	           type:''
-	         }
-	       }
-	     },
-	     methods: {
-	       submitForm(formName) {
-	         this.$refs[formName].validate((valid) => {
-	           if (valid) {
-	             alert('save success!');
-	           } else {
-	             console.log('error submit!!');
-	             return false;
-	           }
-	         });
-	       },
-	       resetForm(formName) {
-	         this.$refs[formName].resetFields();
-	       }
-	     }
-	   }
->>>>>>> parent of abc025a... addmovietype页面完成提交
 	
-</script>
+	.addMovie{
+		color:red;
+	}
+</style>
+
+
+        	
