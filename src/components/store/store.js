b06bd3e1640c98e81300d4311aeed7d1a16e7e0c
Dawn <605722309@qq.com>
2017-03-20 20:20:57 +0800

    no message

--- src/components/store/store.js
+++ src/components/store/store.js
@@ -5,20 +5,20 @@ import createLogger from 'vuex/dist/logger';
 
 Vue.use(Vuex);
 
-<<<<<<< HEAD
-import INFO from './info/info-store.js';
-import MOVIE from './movie/movie-store.js';
+// <<<<<<< HEAD
+// import INFO from './info/info-store.js';
+// import MOVIE from './movie/movie-store.js';
 // import movieTypeList from './movieTypeList/movieTypeList.js';
 
-const store = new Vuex.Store({
-	modules: {
-		INFO,
-		MOVIE/*,
-		movieTypeList*/
-	},
-	mutations: {
+// const store = new Vuex.Store({
+// 	modules: {
+// 		INFO,
+// 		MOVIE/*,
+// 		movieTypeList*/
+// 	},
+// 	mutations: {
 		
-=======
+// =======
 import INFO_store from './info/info-store.js';
 import MOVIE_store from './movie/movie-store.js';
 import MOVIE_LIST_store from './movie/movielist/movielist-store.js';
@@ -28,7 +28,7 @@ const store = new Vuex.Store({
 		INFO_store,
 		MOVIE_store,
 		MOVIE_LIST_store,
->>>>>>> 0dbfe9678cdcb09ff449ee978b26347a870f2015
+// >>>>>>> 0dbfe9678cdcb09ff449ee978b26347a870f2015
 	},
 	plugins: [createLogger()]
 });
