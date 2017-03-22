af7533686c86231b2d11cd4556d3906f1f8c13da
lx <997720962@qq.com>
2017-03-22 17:47:13 +0800

    新增排片加入store

---
+++ src/components/store/schedule/mutations_type.js
@@ -0,0 +1,13 @@
+/**
+ * Created by lyh on 2017/3/22.
+ */
+export const GET_MOVIE='GET_MOVIE';
+export const GET_CINEMAS='GET_CINEMAS';
+export const GET_AUDITORIUM='GET_AUDITORIUM';
+
+export const GET_MOVIEID='GET_MOVIEID';
+export const GET_CINEMAID='GET_CINEMAID';
+export const GET_AUDITORIUMIS='GET_AUDITORIUMIS';
+export const BTN_IS_DISABLED='BTN_IS_DISABLED';
+export const SAVE='SAVE';
+export const RESET='RESET';
--- src/components/store/store.js
+++ src/components/store/store.js
@@ -11,6 +11,7 @@ import INFO_store from './info/info-store.js';
 import MOVIE_store from './movie/movie-store.js';
 import MOVIE_LIST_store from './movie/movielist/movielist-store.js';
 import MOVIE_TYPE_LIST_store from './movie/movietypelist/movietypelist-store.js';
+import ADD_SCHEDULE_store from './schedule/addschedule/addschedule-store.js';
 
 const store = new Vuex.Store({
 	modules: {
@@ -19,6 +20,8 @@ const store = new Vuex.Store({
 
 		MOVIE_LIST_store,
 		MOVIE_TYPE_LIST_store,
+
+        ADD_SCHEDULE_store,
 
 
 	},
