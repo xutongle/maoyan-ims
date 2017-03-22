f3adaf1de23a63f001177492b3d079cc319e5674
Dawn <605722309@qq.com>
2017-03-22 23:03:59 +0800

    store.js 更新后冲突

--- src/components/store/store.js
+++ src/components/store/store.js
@@ -10,12 +10,12 @@ import INFO_store from './info/info-store.js';
 import MOVIE_store from './movie/movie-store.js';
 import MOVIE_LIST_store from './movie/movielist/movielist-store.js';
 import MOVIE_TYPE_LIST_store from './movie/movietypelist/movietypelist-store.js';
-<<<<<<< HEAD
+
 import Movie_TypeList from './movie-type-list/movieTypeList.js'
-=======
+
 import ADD_SCHEDULE_store from './schedule/addschedule/addschedule-store.js';
 import USER_LIST_store from './user/userlist/userlist-store.js';
->>>>>>> fb086a5726041d5be7fd66d1546d08d8a81930e3
+
 
 const store = new Vuex.Store({
 	state:{
@@ -26,12 +26,12 @@ const store = new Vuex.Store({
 		MOVIE_store,
 		MOVIE_LIST_store,
 		MOVIE_TYPE_LIST_store,
-<<<<<<< HEAD
+
 		Movie_TypeList
-=======
+
 		USER_LIST_store,
         ADD_SCHEDULE_store,
->>>>>>> fb086a5726041d5be7fd66d1546d08d8a81930e3
+
 	},
 	plugins: [createLogger()]
 });
